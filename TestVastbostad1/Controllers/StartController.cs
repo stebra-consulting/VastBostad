275de8bd14b884baf21956d26ef9161db1967d59
Simon Bergqvist <simon.bergqvist@stebra.se>
2015-12-07 10:06:22 +0100

    intial commit

---
+++ TestVastbostad1/Controllers/StartController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestVastbostad1.Controllers
+{
+    public class StartController : Controller
+    {
+   
+        public ActionResult Home()
+        {
+            return View();
+        }
+        
+        public ActionResult Bostader()
+        {
+            return View();
+        }
+        public ActionResult Lokaler()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ TestVastbostad1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestVastbostad1.MvcApplication" Language="C#" %>
